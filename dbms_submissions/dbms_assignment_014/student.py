32547347bc4335c09267ce6aee8128108269c522
EC2 Default User <ec2-user@ip-172-31-26-218.ap-southeast-1.compute.internal>
2020-05-28 09:21:00 +0000

    dbm

--- dbms_submissions/dbms_assignment_014/student.py
+++ dbms_submissions/dbms_assignment_014/student.py
@@ -1,5 +1,5 @@
 
-class DoesNotExist(Exception):
+'''class DoesNotExist(Exception):
     pass
 
 class MultipleObjectsReturned(Exception):
@@ -120,8 +120,86 @@ def read_data(sql_query):
     crsr.execute(sql_query) 
     ans= crsr.fetchall() 
     connection.close()
-    return ans
+    return ans'''
 
+class InvalidField(Exception):
+    pass
+class Student:
+    def __init__(self,name,age,score):
+        self.name=name
+        self.student_id=None
+        self.age=age
+        self.score=score
+    @staticmethod
+    def filter(**kwargs):
+        list=[]
+        mem=["student_id","name","age","score"]
+        operations={'lt':'<','lte':'<=','gt':'>','gte':'>=','eq':'=','neq':'!='}
+        for key,value in kwargs.items():
+            l=key.split("__")
+            
+            if l[0] not in mem:
+                raise InvalidField
+            if l[0] in mem and len(l)==1:
+                if l[0]=='name':
+                    query=f"{key}='{value}'"
+                else:
+                    query=f"{key}={value}"
+            elif l[1]=='in':
+                query=f"{l[0]} in {tuple(value)}"
+            elif l[1]=='contains':
+                query=f"{l[0]} Like '%{value}%'"
+            else:
+                query=f"{l[0]}{operations[l[1]]}{value}"
+            list.append(query)
+        return " and ".join(list)
+    
+    @staticmethod
+    def aggregate(op,field,**kwargs):
+        att=["student_id","name","age","score"]
+        if field not in att:
+            raise InvalidField
+        if len(kwargs)==0:
+            q=f"select {op}({field}) from Student"
+        else:
+            cond=Student.filter(**kwargs)
+            q=f"select {op}({field}) from student where {cond}"
+        res=read_data(q)
+        return res[0][0]
+        
+    
+    @classmethod
+    def avg(cls,field,**kwargs):
+        x=cls.aggregate("avg",field,**kwargs)
+        return x
+        
+    @classmethod
+    def min(cls,field,**kwargs):
+        x=cls.aggregate("min",field,**kwargs)
+        return x
+    
+    @classmethod
+    def max(cls,field,**kwargs):
+        x=cls.aggregate("max",field,**kwargs)
+        return x
+    
+    @classmethod
+    def sum(cls,field,**kwargs):
+        x=cls.aggregate("sum",field,**kwargs)
+        return x
+        
+    @classmethod
+    def count(cls,field=None,**kwargs):
+        x=cls.aggregate("count",field,**kwargs)
+        return x
+    
+def read_data(sql_query):
+	import sqlite3
+	connection = sqlite3.connect("students.sqlite3")
+	crsr = connection.cursor() 
+	crsr.execute(sql_query) 
+	ans= crsr.fetchall()  
+	connection.close() 
+	return ans
 
-hhhh	
 https://ap-southeast-1.console.aws .amazon.com/cloud9/ide/5daabffa44814b6ea3fa7a2bdb34e811
